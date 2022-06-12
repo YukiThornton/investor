5ef88a1ec0101a5993ee5bf465d1951c98e86e15
YukiThornton <16602330+YukiThornton@users.noreply.github.com>
2022-06-12 21:00:20 +0900

    Add parser for request json

--- lib/api/router.ex
+++ lib/api/router.ex
@@ -2,6 +2,11 @@ defmodule Api.Router do
   use Plug.Router
   use Plug.ErrorHandler
 
+  plug Plug.Parsers,
+    parsers: [:json],
+    pass: ["application/json"],
+    json_decoder: {Jason, :decode!, [[keys: :atoms]]}
+
   plug(:match)
   plug(:dispatch)
 
@@ -9,13 +14,10 @@ defmodule Api.Router do
     send_resp(conn, 200, "hello")
   end
 
-  get "/plans" do
-    budget = %{max: 50.0, min: 40.0}
-    portfolio = [
-      %{:ticker => "HOG", :price => 10.2, :holdings => 2, :target_ratio => 0.3},
-      %{:ticker => "FUG", :price => 15.1, :holdings => 1, :target_ratio => 0.7}
-    ]
-
+  post "/plans" do
+    %{budget: budget, portfolio: portfolio} = conn.body_params
+    IO.inspect(budget)
+    IO.inspect(portfolio)
     result = App.Investor.suggest_what_to_buy(budget, portfolio)
     send_resp(conn, 200, Jason.encode!(%{"plans" => result}))
   end
